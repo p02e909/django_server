edc6b4f75ef065f6b30e16e49c98c9fa0f56e4e9
nhattao <dangtrangnhattao@gmail.com>
2021-02-16 19:16:37 +0700

    django template

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+	{
+		"author": "nhattao",
+		"title": "blog post 1",
+		"content": "1st post content",
+		"date_posted": "aug 1, 2018"
+	},
+	{
+		"author": "nhattao 2",
+		"title": "blog post 2",
+		"content": "2nd post content",
+		"date_posted": "aug 2, 2018"
+	},
+]
+
+
+def home(request):
+	context = {
+		"posts": posts
+	}
+	return render(request, "blog/home.html", context)
+
+
+def about(request):
+	return render(request, "blog/about.html", {"title": "about"})
